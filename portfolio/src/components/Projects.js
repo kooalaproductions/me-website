3f7dcfcbc08d9810ba8110f510c6e48103065cf2
kooalaproductions <erniesanchez619@gmail.com>
2021-09-16 16:41:58 -0700

    centered cards in projects and added clickebale buttons that open a new window for the live project and github repo

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -5,6 +5,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../css/projects.css';
 
@@ -15,59 +16,78 @@ class Projects extends Component{
         return(
         
             <>
-           <Container>
+           <Container className="full-body-projects-container">
                     <Row>
                     <Navigation></Navigation>
                     </Row>   
-                    <Row xs={1} md={2} className="g-4">
-    <Col>
-      <Card>
-        <Card.Img variant="top" src="holder.js/100px160" />
-        <Card.Body>
-          <Card.Title>Mheels</Card.Title>
-          <Card.Text>
-          Mheels is the one stop app to find your nearest food-truck / food on cart that could be around your corner. Ability to use google maps for directions to food locaiton.
-          </Card.Text>
-        </Card.Body>
-      </Card>
-    </Col>
-    <Col>
-      <Card>
-        <Card.Img variant="top" src="holder.js/100px160" />
-        <Card.Body>
-          <Card.Title>Beer Me</Card.Title>
-          <Card.Text>
-          Social Media Application that helps liquour enthusiasts share their love for a certain brand of alchohol. Also provides an easy library for Alchohol you have never tried but always wanted to
-          </Card.Text>
-        </Card.Body>
-      </Card>
-    </Col>
-</Row>
-<Row xs={1} md={2} className="g-4">
-    <Col>
-      <Card>
-        <Card.Img variant="top" src="holder.js/100px160" />
-        <Card.Body>
-          <Card.Title>Pre-Game</Card.Title>
-          <Card.Text>
-          Pre-Game was created with the intention of you having a great start to your night. Enter the venue/location you'll attend to find all the nearby local spots.
-          </Card.Text>
-        </Card.Body>
-      </Card>
-    </Col>
-    <Col>
-      <Card>
-        <Card.Img variant="top" src="holder.js/100px160" />
-        <Card.Body>
-          <Card.Title>Train Scheduler</Card.Title>
-          <Card.Text>
-          Application that helps the user know when the next train/trolley is coming so they can plan their day accordingly.
-          </Card.Text>
-        </Card.Body>
-      </Card>
-    </Col>
-</Row>
-                   
+                    <Container className="row-container">
+                    <Row v>
+                        
+                            <Col>
+                            <Card>
+                                <Card.Img variant="top" src="holder.js/100px160" />
+                                <Card.Body>
+                                <Card.Title>Mheels</Card.Title>
+                                <Card.Text>
+                                Mheels is the one stop app to find your nearest food-truck / food on cart that could be around your corner. Ability to use google maps for directions to food locaiton.
+                                </Card.Text>
+                                <Container className="button-container">
+                                <Button onClick={()=> window.open("https://mheels.herokuapp.com/", "_blank")}>Live Project</Button>
+                                <Button onClick={()=> window.open("https://github.com/rgonz166/project3", "_blank")}>Github</Button>
+                                </Container>
+                                
+                                </Card.Body>
+                            </Card>
+                            </Col>
+                            <Col>
+                            <Card>
+                                <Card.Img variant="top" src="holder.js/100px160" />
+                                <Card.Body>
+                                <Card.Title>Beer Me</Card.Title>
+                                <Card.Text>
+                                Social Media Application that helps liquour enthusiasts share their love for a certain brand of alchohol. Also provides an easy library for Alchohol you have never tried but always wanted to
+                                </Card.Text>
+                                <Container className="button-container">
+                                <Button onClick={()=> window.open("http://beerme-ucsd.herokuapp.com/", "_blank")}>Live Project</Button>
+                                <Button onClick={()=> window.open("https://github.com/rgonz166/BeerMe", "_blank")}>Github</Button>
+                                </Container>
+                                </Card.Body>
+                            </Card>
+                            </Col>
+                        </Row>
+                        <Row v>
+                            <Col>
+                            <Card>
+                                <Card.Img variant="top" src="holder.js/100px160" />
+                                <Card.Body>
+                                <Card.Title>Pre-Game</Card.Title>
+                                <Card.Text>
+                                Pre-Game was created with the intention of you having a great start to your night. Enter the venue/location you'll attend to find all the nearby local spots.
+                                </Card.Text>
+                                <Container className="button-container">
+                                <Button onClick={()=> window.open("https://rgonz166.github.io/Pre-Game/", "_blank")}>Live Project</Button>
+                                <Button onClick={()=> window.open("https://github.com/rgonz166/Pre-Game", "_blank")}>Github</Button>
+                                </Container>
+                                </Card.Body>
+                            </Card>
+                            </Col>
+                            <Col>
+                            <Card>
+                                <Card.Img variant="top" src="holder.js/100px160" />
+                                <Card.Body>
+                                <Card.Title>Train Scheduler</Card.Title>
+                                <Card.Text>
+                                Application that helps the user know when the next train/trolley is coming so they can plan their day accordingly.
+                                </Card.Text>
+                                <Container className="button-container">
+                                <Button onClick={()=> window.open("https://kooalaproductions.github.io/TrainTime/", "_blank")}>Live Project</Button>
+                                <Button onClick={()=> window.open("https://github.com/kooalaproductions/TrainTime", "_blank")}>Github</Button>
+                                </Container>
+                                </Card.Body>
+                            </Card>
+                            </Col>
+                    </Row>
+                    </Container>
                 </Container>
             </>
         )
