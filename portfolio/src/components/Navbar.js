331dd49a7bdef0b60622a2b3f56ebe992562a676
Sky <erniesanchez619@gmial.com>
2021-09-23 16:14:58 -0700

    added an onClick event handler to change the color of the navbar links but it only works for a second because the page is reloaded so the css changes do not work

--- portfolio/src/components/Navbar.js
+++ portfolio/src/components/Navbar.js
@@ -11,19 +11,24 @@ import LinkedInIcon from '@material-ui/icons/LinkedIn';
 
 class Navigation extends Component{
 
-  jQueryCode = () =>{
-    $(".home").on('click',function(){
-      console.log("jquery works");//this works
-      $(".navbar").css('background-color', 'red')//this doesnt work
-     
-     });
-   }
-   componentDidMount(){
-     this.jQueryCode()
-   }
+  constructor(props) {
+    super(props);
+    this.state = {
+      background: '',
+      color:''
+  };
 
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
+  }
 
+  handleClick() {//this does not work bc onclick reloads page so the new changes disappear
+    this.setState({background: 'green', color: 'red'});// here I want to change the color to red
+    alert('onClick works');
+  }
     render(){
+
+      const { background, color } = this.state;
         return(
             
         <Navbar bg="light" expand="lg" fixed="top">
@@ -34,8 +39,8 @@ class Navigation extends Component{
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link className='home' href="/" >HOME</Nav.Link>
-              <Nav.Link className='projects' href="/projects" >PROJECTS</Nav.Link>
+              <Nav.Link className='home' href="/" onClick={this.handleClick} style={{background: background, color: color}}>HOME</Nav.Link>
+              <Nav.Link className='projects' href="/projects" onClick={this.handleClick} style={{background: background, color: color}}>PROJECTS</Nav.Link>
               <Nav.Link className='about' href="/about" >ABOUT</Nav.Link>
               <Nav.Link className='contact' href="/contact" >CONTACT</Nav.Link>
 
