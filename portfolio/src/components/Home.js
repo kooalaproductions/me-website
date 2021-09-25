04fa967359978618efbf4df131b9dbb22596609d
Sky <erniesanchez619@gmial.com>
2021-09-25 02:26:23 -0700

    added class names to card properties because it was affecting the project cards

--- portfolio/src/components/Home.js
+++ portfolio/src/components/Home.js
@@ -34,7 +34,7 @@ class Home extends Component{
 
                     <Col xs={8}>
                     <Card className="card-whole">
-                        <Card.Body>
+                        <Card.Body className="body-of-card">
                             <Card.Title className='name-title'>Hello I am Ernesto Sanchez</Card.Title>
                             <Card.Text className='school-title'>
                             Bachelor of Science in Computer Science from San Diego State University
