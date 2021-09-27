c3f03dd65fd9bb15d1d222d502e7aac58341df98
Sky <erniesanchez619@gmial.com>
2021-09-27 01:58:42 -0700

    working on homepage editing

--- portfolio/src/components/Home.js
+++ portfolio/src/components/Home.js
@@ -24,7 +24,7 @@ class Home extends Component{
         return(
         
             <>
-           <Container>
+           <Container className='home-container'>
                     <Row>
                     <Navigation></Navigation>
                     </Row>   
@@ -32,26 +32,32 @@ class Home extends Component{
                     <Container className='profile-container'>
                         <Row className="profile-row">
 
-                    <Col xs={8}>
-                    <Card className="card-whole">
-                        <Card.Body className="body-of-card">
-                            <Card.Title className='name-title'>Hello I am Ernesto Sanchez</Card.Title>
-                            <Card.Text className='school-title'>
-                            Bachelor of Science in Computer Science from San Diego State University
-                            </Card.Text>
-                            <Card.Text className='location-title'>
-                            San Diego, CA
-                            </Card.Text>
-                        </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col xl={4}><Card.Img className="profile-pic" variant="top" src={portfolioPic} alt="Avatar"/></Col>
-                        
-                   
-                            
+                            <Col xs={8}>
+                            <Card className="card-whole">
+                                <Card.Body className="body-of-card">
+                                    <Card.Title className='name-title'>Hello I am Ernesto Sanchez</Card.Title>
+                                    <Card.Text className='school-title'>
+                                    Bachelor of Science in Computer Science from San Diego State University
+                                    </Card.Text>
+                                    <Card.Text className='location-title'>
+                                    San Diego, CA
+                                    </Card.Text>
+                                </Card.Body>
+                                </Card>
+                            </Col>
+                            <Col xl={4}><Card.Img className="profile-pic" variant="top" src={portfolioPic} alt="Avatar"/></Col>
                     
                         </Row>
                     </Container>
+
+                    <Container className='footer-home'>
+                        <Row>
+                            <h1 className='footer-header'>FOOTER</h1>
+                            <h1 className='footer-header'>FOOTER</h1>
+                            <h1 className='footer-header'>FOOTER</h1>
+                        </Row>
+                    </Container>
+                   
                    
                 </Container>
             </>
