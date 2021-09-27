26b326d4e83b4cc359a8667f63c135677eda4896
Sky <erniesanchez619@gmial.com>
2021-09-27 16:52:21 -0700

    added backgrounf as an image

--- portfolio/src/components/Home.js
+++ portfolio/src/components/Home.js
@@ -5,9 +5,14 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom'
+import Image from 'react-bootstrap/Image'
+import backgroundLake from '../images/lakebackground.jpeg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../css/home.css';
 import portfolioPic from '../images/portfolio-pic.jpeg';
+
 
 function Example() {
     const [show, setShow] = useState(false);
@@ -24,12 +29,15 @@ class Home extends Component{
         return(
         
             <>
+                         <Image className ='lake-img' src={backgroundLake} />
+
            <Container className='home-container'>
                     <Row>
                     <Navigation></Navigation>
                     </Row>   
-                   
+                  
                     <Container className='profile-container'>
+                        
                         <Row className="profile-row">
 
                             <Col xs={8}>
@@ -42,19 +50,16 @@ class Home extends Component{
                                     <Card.Text className='location-title'>
                                     San Diego, CA
                                     </Card.Text>
+                                    <Button className='button-projects'>
+                                    <Link className = 'projects-button' to='/projects'> PROJECTS</Link>
+                                    </Button>{' '}
                                 </Card.Body>
                                 </Card>
                             </Col>
                             <Col xl={4}><Card.Img className="profile-pic" variant="top" src={portfolioPic} alt="Avatar"/></Col>
                     
-                        </Row>
-                    </Container>
-
-                    <Container className='footer-home'>
-                        <Row>
-                            <h1 className='footer-header'>FOOTER</h1>
-                            <h1 className='footer-header'>FOOTER</h1>
-                            <h1 className='footer-header'>FOOTER</h1>
+                           
+                            
                         </Row>
                     </Container>
                    
