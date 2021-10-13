b8e6dd85beb4333c975485a437989b4ce472c3b5
Sky <erniesanchez619@gmial.com>
2021-10-13 00:08:08 -0500

    added summary about me

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -28,14 +28,28 @@ class About extends Component{
                          </Row>
                          <Row>
                              <Col>
+                             <div className='intro'>
+                             Thank you for visiting my portfolio. I am currently looking for jobs in computer science or information technology.
+                             I graduated from San Diego State University with a degree in computer science and during that time I was an I.T. Student Worker
+                             for the Department of Child Support.
+                                 </div>
+                            
+                             <br></br>
+                             
+                             <div className='future'>
+                             I will continue working on personal projects to continuously work on my skills.
+                             My goal is to become a Lead Software Developer or a Senior I.T. Engineer.
+                             A programming language that I hope to learn is python because I want to
+                             expand my knowledge and be flexible in any job position. I will work on python projects and post them on my portfolio in the future. 
 
-                             Thank you for visiting my portfolio. I am currently looking for jobs in computer science. 
 
+                             </div>
                              </Col>
                              <Col>
                              My Skills
                              </Col>
                          </Row>
+
                      </Container>
                     
                  </Container>
