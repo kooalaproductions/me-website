131b84e8bdd771f05fb2e5c153f459c0ddbae327
Sky <erniesanchez619@gmial.com>
2021-10-14 16:38:40 -0500

    nodemailer is not working properly there is a 404 issue

--- portfolio/src/components/Contact.js
+++ portfolio/src/components/Contact.js
@@ -3,6 +3,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Navigation from './Navbar';
+import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../css/contact.css';
 
@@ -10,6 +11,51 @@ import '../css/contact.css';
  
 class Contact extends Component{
 
+    constructor(props) {
+        super(props);
+        this.state = {
+          name: '',
+          email: '',
+          subject:'',
+          message: ''
+        }
+    }
+
+    onNameChange(event) {
+        this.setState({name: event.target.value})
+    }
+
+    onEmailChange(event) {
+        this.setState({email: event.target.value})
+    }
+
+    onSubjectChange(event) {
+        this.setState({subject: event.target.value})
+    }
+
+    onMsgChange(event) {
+        this.setState({message: event.target.value})
+    }
+
+    submitEmail(e){
+        e.preventDefault();
+        axios({
+          method: "POST", 
+          url:"/send", 
+          data:  this.state
+        }).then((response)=>{
+          if (response.data.status === 'success'){
+              alert("Message Sent."); 
+              this.resetForm()
+          }else if(response.data.status === 'fail'){
+              alert("Message failed to send.")
+          }
+        })
+}
+
+resetForm(){
+        this.setState({name: '', email: '',subject:'', message: ''})
+}
     render(){
         return (
             <>
@@ -20,13 +66,48 @@ class Contact extends Component{
                      <Row></Row>
                    
  
-                     <Container>
-                         <Row>
-                             <Col>
-                             <h1 className='contact-title'>CONTACT PAGE</h1>
-                                 </Col>
-                         </Row>
-                     </Container>
+                     <div className="section">
+                        <div className="container-email">
+                            <div className="row">
+                                <div className="col-md-12">
+                                    <div className="section-title">
+                                        <form id="contact-form" onSubmit={this.submitEmail.bind(this)} 
+                                            method="POST">
+                                        <div className="form-group">
+                                        <div className="row">
+                                        <div className="col-md-6">
+                                            <input placeholder = "Name"  id="name" type="text" 
+                                            className="form-control" required value={this.state.name} 
+                                            onChange={this.onNameChange.bind(this)}/>
+                                        </div>
+                                        <div className="col-md-6">
+                                            <input placeholder = "Email"  id="email" type="email"
+                                            className="form-control" aria-describedby="emailHelp"
+                                            required value={this.state.email} onChange=
+                                            {this.onEmailChange.bind(this)}/>
+                                        </div>
+                                        </div>
+                                        </div>
+                                        <div className="form-group">
+                                            <input placeholder = "Subject"  id="subject" type="text"
+                                            className="form-control" required value={this.state.subject}
+                                            onChange={this.onSubjectChange.bind(this)}/>
+                                        </div>
+                                        <div className="form-group">
+                                            <textarea placeholder = "Message"  id="message" 
+                                            className="form-control" rows="1" 
+                                            required value={this.state.message}
+                                            onChange= {this.onMsgChange.bind(this)}/>
+                                        </div>
+                                        <button type="submit" className="primary-btn submit">Submit</button>
+                                        </form>
+                                    </div>
+                                </div>
+
+                            </div>
+
+                </div>
+            </div>
                     
                  </Container>
              </>
